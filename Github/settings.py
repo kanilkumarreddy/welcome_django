d593cad346aa5d50d7e8065223f58f9b6b0621ab
kanilkumarreddy <116055966+kanilkumarreddy@users.noreply.github.com>
2022-10-21 12:05:09 +0530

    change

--- Github/settings.py
+++ Github/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django-cors-headers',
     'django.contrib.staticfiles',
     "django.core.management",
     'sample_git'
